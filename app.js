a7c74f73908b8ecc94142567c6765ff68309bf78
KOSTSOV Glib <glibkostsov@gmail.com>
2019-01-31 14:38:33 +0100

    final project

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 var express = require("express"),
     app = express(),
     todoRoutes = require('./routes/todos'),
-    bodyParser = require('body-parser')
-    ;
+    bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true }));
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/views"));
+
 
 app.get("/", function(req, res){
-    res.send("hello from the landing page");
+    res.sendFile("index.html");
 });
+
 
 app.use("/api/todos", todoRoutes);
 
---
+++ public/app.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+    $.getJSON("/api/todos")
+    .then(addTodos)
+
+    $("#todoInput").keypress(function(event){
+        if(event.which == 13 ) {
+            createTodo();
+        }
+    });
+
+    $(".list").on("click", "li", function(){
+        updateTodo($(this));
+    })
+
+    $(".list").on("click", "span" ,function(event){
+        event.stopPropagation();
+        removeTodo($(this).parent());
+    });
+
+
+
+});
+
+function addTodos(todos){
+    todos.forEach(function(todo){
+       addTodo(todo);  
+    })
+}
+
+function addTodo(todo){
+    var newTodo = $("<li class = 'task' >"+todo.name + " <span>X</span></li>");
+    console.log(todo);
+    newTodo.data("id", todo._id);
+    newTodo.data("completed", todo.completed);
+    if(todo.completed){
+        newTodo.addClass("done");
+    }
+    $(".list").append(newTodo);
+}
+
+function createTodo(){
+    var usrInput = $("#todoInput").val();
+    console.log(usrInput);
+    $.post("/api/todos", {name: usrInput })
+    .then(function(newTodo){
+        $("#todoInput").val("");
+        addTodo(newTodo);
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+}
+
+function removeTodo(todo){
+        var clickedId = todo.data("id");
+        $.ajax({
+            method: "DELETE",
+            url: "/api/todos/" + clickedId
+        })
+        .then(function(data){
+            todo.remove() ;
+        })         
+}
+
+function updateTodo(todo){
+    var clickedId = todo.data("id");
+    var updateUrl  = "/api/todos/" + clickedId;
+    var isDone = !todo.data("completed");
+    console.log(isDone);
+    var updateData = {completed : isDone}
+    $.ajax({
+        method: "PUT",
+        url: updateUrl,
+        data: updateData
+    })
+    .then(function(updatedTodo){
+        todo.toggleClass("done");
+        todo.data("completed", isDone)
+    })
+}
