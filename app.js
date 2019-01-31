3f5b887a15a5384a4dd95d3698ed86132b2878d7
KOSTSOV Glib <glibkostsov@gmail.com>
2019-01-31 11:56:15 +0100

    complete RESTfull routes

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require("express"),
     app = express(),
-    todoRoutes = require('./routes/todos');
+    todoRoutes = require('./routes/todos'),
+    bodyParser = require('body-parser')
     ;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true }));
 
 app.get("/", function(req, res){
     res.send("hello from the landing page");
--- routes/todos.js
+++ routes/todos.js
@@ -1,18 +1,17 @@
 var express = require("express"),
     router =  express.Router();
     
- var  db = require("../models");
+ var  db = require("../models"),
+      helpers = require("../helpers/todos");
 
-router.get("/", function(req,res){
-    db.Todo.find()
-    .then(function(todos){
-        res.json(todos);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-});    
+router.route("/")
+.get(helpers.getTodos)
+.post(helpers.createTodo)
 
+router.route("/:todoId")
+.get(helpers.getTodo)
+.put(helpers.updateTodo)
+.delete(helpers.deleteTodo)
 
 module.exports = router;
 
